0d5115c3fe1bce0b870588673d028dea987e2e84
aafedorov2694 <antonfedorov2694@gmail.com>
2022-02-24 18:53:40 +0200

    exercise 1.11 is done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,20 +1,53 @@
 import { useState } from "react";
 
-const Statistics = (props) => {
- 
+const StatistisLine = (props) => {
 
-  let badCount = props.bad*-1
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              {props.text}
+            </td>
+            <td>
+              {props.value}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+  );
+}
+
+const Button = (props) => {
+
+  return (
+    <div>
+      <button onClick={props.function}>{props.text}</button>
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+
+
+  let badCount = props.bad * -1
   console.log('statistics bad: ', props.bad, 'badCount: ', badCount)
 
   let sum = (props.good + props.bad + props.neutral);
-  let avrg = (props.good+badCount)/props.total;
-  let positive = props.good/sum*100;
+  let avrg = (props.good + badCount) / props.total;
+  let positive = props.good / sum * 100;
 
-  return(
+  return (
     <div>
-      <p>all: {sum}</p>
-      <p>average: {avrg}</p>
-      <p>positive: {positive}%</p>
+      <StatistisLine text='good: ' value={props.good} />
+      <StatistisLine text='neutral: ' value={props.neutral} />
+      <StatistisLine text='bad: ' value={props.bad} />
+      <StatistisLine text='all: ' value={sum} />
+      <StatistisLine text='average: ' value={avrg.toFixed(1)} />
+      <StatistisLine text='positive: ' value={positive.toFixed(0) + '%'} />
     </div>
   )
 }
@@ -25,56 +58,76 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [total, setTotal] = useState(0);
-  
-  
-  
+
 
   const plusGood = () => {
     setGood(good + 1);
-    setTotal(total+1);
+    setTotal(total + 1);
   }
   const plusNeutral = () => {
     setNeutral(neutral + 1);
-    setTotal(total+1);
+    setTotal(total + 1);
 
   }
   const plusBad = () => {
     setBad(bad + 1);
-    setTotal(total+1);
+    setTotal(total + 1);
 
   }
 
-  if(good === 0 && bad === 0 && neutral === 0)  {
-    return(
+  if (good === 0 && bad === 0 && neutral === 0) {
+    return (
       <div>
-         <h2>Give feedback</h2>
-          <button onClick ={plusGood}>good</button>
-          <button onClick ={plusNeutral}>neutral</button>
-          <button onClick ={plusBad}>bad</button>
-        <h2>Statistics</h2> 
-          <p>No feedback given</p>
+        <h2>Give Feedback</h2>
+        <table>
+          <tbody>
+            <tr>
+              <td style={{width: '10px'}}>
+                <Button text='good' function={plusGood} />
+              </td>
+              <td style={{width: '10px'}}>
+                <Button text='neutral' function={plusNeutral} />
+              </td>
+              <td style={{width: '10px'}}>
+                <Button text='bad' function={plusBad} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+    
+        <h2>Statistics</h2>
+        <p>No feedback given</p>
       </div>
     )
-  } else {  
+  } else {
 
-  return (
-    <div>
-      <h2>Give feedback</h2>
-      <button onClick ={plusGood}>good</button>
-      <button onClick ={plusNeutral}>neutral</button>
-      <button onClick ={plusBad}>bad</button>
-      {console.log('Good: ', good, ' Neutral: ', neutral,  ' Bad: ', bad, ' Total: ', total)}
-    
-      <h2>Statistics</h2>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      
-      <Statistics good = {good} neutral = {neutral} bad = {bad} total={total}/>
-     
+    return (
+     <div>
+            <h2>Give Feedback</h2>
+            <table>
+              <tbody>
+                <tr>
+                  <td style={{width: '10px'}}>
+                    <Button text='good' function={plusGood} />
+                  </td>
+                  <td style={{width: '10px'}}>
+                    <Button text='neutral' function={plusNeutral} />
+                  </td>
+                  <td style={{width: '10px'}}>
+                    <Button text='bad' function={plusBad} />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
 
-    </div>
-  );
+            
+            <h2>Statistics</h2>
+        
+              <Statistics good={good} neutral={neutral} bad={bad} total={total} />
+          
+      </div>
+    );
   }
 }
 
