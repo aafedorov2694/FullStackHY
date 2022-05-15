e5550fe90ada3332f207489464a09d16400fdb50
aafedorov2694 <antonfedorov2694@gmail.com>
2022-05-15 20:10:51 +0300

    Tasks 2.13-2.14 done. Part2 Finished

--- part2/countries/src/components/countryInput.jsx
+++ part2/countries/src/components/countryInput.jsx
@@ -1,35 +1,39 @@
 import React from "react";
 
 const CountryInput = (props) => {
-   
-    const {
-        countryName,
-        setFoundCountries
-    } = props;
-    let selectedCountries = []
-    let uploadCheck = Boolean
-    const search = (event) => {
-       let target = event.target.value;
-        selectedCountries = countryName.filter(country => {
-            if(country.toLowerCase().includes(event.target.value) === true){
-               return country;
-           }
-        })
-        
-        setFoundCountries(selectedCountries);
-        console.log('countryName.length: ', countryName.length)
-        console.log('event target value: ', target)
-    }
-    
-    countryName.length === 0 ? uploadCheck = true : uploadCheck = false
+  const { countryName, setFoundCountries } = props;
+  let selectedCountries = [];
+  let uploadCheck = Boolean;
+  const search = (event) => {
+    event.preventDefault();
+    console.log("event: ", event.target.value.toLowerCase());
+    selectedCountries = countryName.filter((country) => {
+      if (
+        country.name.common
+          .toLowerCase()
+          .includes(event.target.value.toLowerCase()) === true
+      ) {
+        return country;
+      }
+    });
 
-   
-   
-    return(
-        <div>
-            <p>find country: <input placeholder="Type country name" onChange ={search} disabled ={uploadCheck}  /> </p>
-        </div>
-    )
-}
+    setFoundCountries(selectedCountries);
+  };
+
+  countryName.length === 0 ? (uploadCheck = true) : (uploadCheck = false);
+
+  return (
+    <div>
+      <p>
+        find country:{" "}
+        <input
+          placeholder="Type country name"
+          onChange={search}
+          disabled={uploadCheck}
+        />{" "}
+      </p>
+    </div>
+  );
+};
 
 export default CountryInput;
